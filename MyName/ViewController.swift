2b01d8b39d202921f5e9bad69eb9eac59ef4e2c1
Denis <den88000@gmail.com>
2022-08-01 11:59:45 +0300

    add new scene to project

--- MyName/ViewController.swift
+++ MyName/ViewController.swift
@@ -8,10 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func showMessage(){
+        print("you pressed First button")
     }
 }
 
